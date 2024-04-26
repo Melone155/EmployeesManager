52ec7daf1bd134e73eabc157b1ca4f41b712eb57
Melone155 <101599412+Melone155@users.noreply.github.com>
2024-04-26 19:33:23 +0200

    Initial commit

---
+++ MainProgamm.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+#Create Window
+root = tk.Tk()
+root.title("Mitarbeiter Management Programm")
+
+#Windows Size
+root.geometry("900x600")
+
+#Start loop for View Window
+root.mainloop()
---
+++ YML.py
@@ -0,0 +1,7 @@
+import yaml
+
+def CreateConfigs():
+
+    # Path to the YAML files
+    MySQL = 'Config/MySQL.yaml'
+    Employees = 'Config/Employees.yaml'
